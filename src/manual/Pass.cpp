e1d961264b77bd61e51a3755de71afe48d38f9ed
rlaalstn0107 <minsu980107@naver.com>
2023-03-29 19:57:37 +0900

    명령어 구현중

--- src/manual/Pass.cpp
+++ src/manual/Pass.cpp
@@ -6,7 +6,7 @@
 /*   By: mikim3 <mikim3@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:53:08 by mikim3            #+#    #+#             */
-/*   Updated: 2023/03/29 19:40:14 by mikim3           ###   ########.fr       */
+/*   Updated: 2023/03/29 19:57:22 by mikim3           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ void    Pass::Execute(Client* client, std::vector<std::string> args)
 {
     if (args.empty())
     {
-        clinet->SendErrorToClient(Log::GetERRNEEDMOREPARAMS(client->GetNickname(), "PASS"));
+        client->SendErrorToClient(Log::GetERRNEEDMOREPARAMS(client->GetNickname(), "PASS"));
         return ;
     }
 
-    if (client->IsRegistered())
+    // client->IsRegistered()이거 였는데
+    // REGISTERED , Login
+    if (client->GetState() == REGISTERED || client->GetState() == LOGIN)
     {
         client->SendErrorToClient(Log::GetERRALREADYREGISTRED(client->GetNickname()));
         return ;
@@ -35,7 +37,7 @@ void    Pass::Execute(Client* client, std::vector<std::string> args)
 
     if (mServer->GetPassword() != args[0].substr(args[0][0] == ':' ? 1 : 0))
     {
-        client->SendErrorToClient(ERR_PASSWDMISMATCH(client->GetNickname()));
+        client->SendErrorToClient(Log::GetERRPASSWDMISMATCH(client->GetNickname()));
         return ;
     }
 
