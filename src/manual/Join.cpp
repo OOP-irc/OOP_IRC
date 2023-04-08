bdfa71ec10b3ade31fd08ef5f7110f9083a893b6
Joo-hyun-Kang <autoba9687@gmail.com>
2023-04-08 16:04:29 +0900

    fix: PASS-USER-NICK 명령어 중 1개라도 되지 않는 다면 join 할 수 없게 수정

--- src/manual/Join.cpp
+++ src/manual/Join.cpp
@@ -19,9 +19,15 @@ void    Join::Execute(Client* client, std::vector<std::string> args)
         return;
     }
 
+    if (client->GetState() != REGISTERED)
+    {
+        /* 유저가 아직 등록되지 않았음을 알림 */
+        client->SendErrorToClient(Log::GetERRNOTREGISTERED(client->GetPrefix(), client->GetNickname()));
+        return;
+    }
+
     std::string name = args[0];
     std::string pass = args.size() > 1 ? args[1] : "";
-
 
     // 채널을 서버에서 받아오거나 생성한다
     Channel *chan = mServer->GetChannel(name);
